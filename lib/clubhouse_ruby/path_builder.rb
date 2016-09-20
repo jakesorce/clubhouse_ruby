9614ff5c6c813731cb04f0716987eaa654347713
Philip Castiglione <philipcastiglione@gmail.com>
2016-09-20 16:50:59 +1000

    refactored path builder

--- lib/clubhouse_ruby/path_builder.rb
+++ lib/clubhouse_ruby/path_builder.rb
@@ -6,35 +6,49 @@ module ClubhouseRuby
       end
     end
 
+    def clear_path
+      self.path = []
+    end
+
     def method_missing(name, *args)
-      if ClubhouseRuby::METHODS.keys.include?(name)
-        req = ClubhouseRuby::Request.new(
-          self, 
-          method: ClubhouseRuby::METHODS[name],
-          params: args.first
-        )
-        clear_path
-        req.fetch
-      elsif ClubhouseRuby::RESOURCES.include?(name)
+      if known_method?(name)
+        execute_request(name, args.first)
+      elsif known_resource?(name)
         build_path(name, args.first)
-        self
       else
         super
       end
+    end
+
+    def respond_to_missing?(name, include_private = false)
+      ClubhouseRuby::METHODS.keys.include?(name) || ClubhouseRuby::RESOURCES.include?(name) || super
+    end
+
+    private
+
+    def known_method?(name)
+      ClubhouseRuby::METHODS.keys.include?(name)
+    end
+
+    def known_resource?(name)
+      ClubhouseRuby::RESOURCES.include?(name)
+    end
+
+    def execute_request(method, params)
+      req = ClubhouseRuby::Request.new(
+        self, 
+        method: ClubhouseRuby::METHODS[method],
+        params: params
+      )
+      clear_path
+      req.fetch
     end
 
     def build_path(resource, id)
       self.path ||= []
       self.path << resource
       self.path << id if id
-    end
-
-    def clear_path
-      self.path = []
-    end
-
-    def respond_to_missing?(name, include_private = false)
-      ClubhouseRuby::METHODS.keys.include?(name) || ClubhouseRuby::RESOURCES.include?(name) || super
+      self
     end
   end
 end
