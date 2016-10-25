d866877854313bc1222df6a570005829f41097dc
Philip Castiglione <philipcastiglione@gmail.com>
2016-10-25 13:52:27 +1100

    added vcr cassettes to the repo for specs, matching pattern allows removing token

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,9 +11,10 @@ Dotenv.load
 # Configures VCR, which provides responses to our API calls
 # https://github.com/vcr/vcr
 VCR.configure do |config|
-  config.cassette_library_dir = 'spec/vcr_casettes'
+  config.cassette_library_dir = 'spec/vcr_cassettes'
   config.hook_into :webmock
   config.configure_rspec_metadata!
+  config.default_cassette_options = { match_requests_on: [:method, :host, :path] }
 end
 
 require 'clubhouse_ruby'
